43d76f05e9096f5753876837a8f76ede6b6a7599
Rafael Cartenet <rafael.cartenet@insa-rouen.fr>
2016-04-09 17:11:06 +0200

    Face Drawing

---
+++ Python3 - Face Drawing.py
@@ -0,0 +1,8 @@
+hair, cheek, eye, nose, mouth, chin = input().split()
+
+print(5*hair)
+print(cheek+eye+" "+eye+cheek)
+print(cheek+" "+nose+" "+cheek)
+print(cheek+" "+mouth+" "+cheek)
+vide=(5-len(chin))//2
+print(vide*" "+chin)
